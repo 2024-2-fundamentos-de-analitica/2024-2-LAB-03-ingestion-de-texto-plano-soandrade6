938a649c1717645de3695cc3b7cb66376312ac84
Sofia Andrade <sofia.andradep06@gmail.com>
2025-02-16 16:11:48 -0500

    Lab 02 completed

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,7 +3,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 # pylint: disable=import-outside-toplevel
-
+import pandas as pd
 
 def pregunta_01():
     """
@@ -18,3 +18,36 @@ def pregunta_01():
 
 
     """
+    filename = "files/input/clusters_report.txt"
+    
+    with open(filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()[4:]
+
+    clusters = []
+    temp_cluster = []
+
+    for line in lines:
+        line = line.strip()
+
+        if line:
+            parts = line.split()
+            if not temp_cluster:
+                temp_cluster = [
+                    int(parts[0]),
+                    int(parts[1]),
+                    float(parts[2].replace(',', '.')),
+                    " ".join(parts[4:])
+                ]
+            else:
+                temp_cluster[3] += " " + " ".join(parts)
+        else:
+            if temp_cluster:
+                temp_cluster[3] = temp_cluster[3].replace('.', '')
+                clusters.append(temp_cluster)
+                temp_cluster = []
+
+    if temp_cluster:
+        temp_cluster[3] = temp_cluster[3].replace('.', '')
+        clusters.append(temp_cluster)
+
+    return pd.DataFrame(clusters, columns=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
